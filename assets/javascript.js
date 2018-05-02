779021f3e367ca7f23f355c913bb277fda268669
Max Wang <maxwellswang@gmail.com>
2018-05-02 16:52:10 -0400

    css transitions on scroll and hover

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,13 +2,42 @@ $(document).ready(function(){
     $(".parallax").parallax();
     $(".carousel").carousel();
     carousel();
+    $(window).on("scroll resize", checkView);
+    
+    $(window).trigger("scroll");
 });
 
+var $animationElements = $(".animation");
+var $window = $(window);
+
+// materialize carousel
 function carousel() {
     $(".carousel").carousel("next");
     setTimeout(carousel, 1000);
 };
 
+
+
+function checkView() {
+  var windowHeight = $window.height();
+  var windowTop = $window.scrollTop();
+  var windowBottom = (windowTop + windowHeight);
+
+  $.each($animationElements, function() {
+    var $element = $(this);
+    var elementHeight = $element.outerHeight();
+    var elementTop = $element.offset().top;
+    var elementBottom = (elementTop + elementHeight);
+
+    if ((elementTop <= windowBottom) && (elementBottom >= windowTop)) {
+      $element.addClass("in-view");
+    } else {
+      $element.removeClass("in-view");
+    }
+  })
+}
+
+// Smooth scrolling for page navigation
 // Select all links with hashes
 $('a[href*="#"]')
 // Remove links that don't actually link to anything
@@ -38,3 +67,4 @@ $('a[href*="#"]')
       });
     }
 });
+// end scrolling animation
